cd327a274f63c6392a5985971fd3d7313b00b140
mohammad.ewaz@sciencespo.fr <mohammad.ewaz@sciencespo.fr>
2021-11-15 16:18:44 +0100

    user model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_143747) do
+ActiveRecord::Schema.define(version: 2021_11_15_151632) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,10 +19,10 @@ ActiveRecord::Schema.define(version: 2021_11_15_143747) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
-    t.integer "password"
     t.string "profile_picture"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password"
   end
 
 end
