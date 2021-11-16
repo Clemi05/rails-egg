86bd28cac98f681a803bc2fcf98e4c4fac301c38
mohammad.ewaz@sciencespo.fr <mohammad.ewaz@sciencespo.fr>
2021-11-16 11:57:31 +0100

    details added to the show  page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'hens/index'
+  get 'hens/show'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :hens
 end
