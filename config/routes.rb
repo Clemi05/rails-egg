d5b1c5be91b05c1c10390f740b9d817e62602b7a
Clement Azalbert <clementazalbert04@gmail.com>
2021-11-16 11:56:04 +0100

    Created controller booking and updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  ressources :hens do
+    ressources :booking, only: %i[new create]
+  end
 end
