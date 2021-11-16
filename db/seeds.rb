d6835cdf787d7b3dea790026afce63665c487bc8
Clement Azalbert <clementazalbert04@gmail.com>
2021-11-16 12:18:33 +0100

    Routes updated, seed db and updated views/hens/show

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning up database..."
+Hen.destroy_all
+puts "Database cleaned"
+
+Hen.create(name: "Crazy Chicken", description: "This is a crazy chickent! The craziest breed of chicken of the world", rating: 4.6, price_per_day: 20, breed: "Crazy Chicken", eggs_per_day: 10)
+
+puts 'Done!'
