610125794c8b8623872b6e5b1283300f28e35c4e
Clement Azalbert <clementazalbert04@gmail.com>
2021-11-16 17:36:19 +0100

    Hens show done, booking controller & view done and working

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   resources :hens do
-    resources :booking, only: %i[new create]
+    resources :bookings, only: %i[new create]
   end
 end
