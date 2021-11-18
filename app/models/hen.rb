e60225b61d434ed83d4736411b72aa16e7b60e4c
Charlotte Smets <charlotte@smets.lu>
2021-11-18 12:27:20 +0100

    solved issue on hen model

--- app/models/hen.rb
+++ app/models/hen.rb
@@ -2,7 +2,6 @@ class Hen < ApplicationRecord
   has_many :bookings
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
-=======
 
   include PgSearch::Model
   pg_search_scope :search_by_name_and_breed,
