bc0779eb0a46866cf72effd0078debe59f73b79d
Clement Azalbert <clementazalbert04@gmail.com>
2021-11-19 12:20:16 +0100

    booking price with stimulus

--- app/javascript/controllers/price_controller.js
+++ app/javascript/controllers/price_controller.js
@@ -1,18 +1,15 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["start_date", "end_date", "price"]
+  static targets = ["start-date", "end-date", "price"]
 
   updatePrice() {
-    // this.priceTarget.textContent =
-    //   `Hello, ${this.startDateTarget.value}!`
-    console.log("yes bitch")
-    // `${(this.startDateTarget.value - this.endDateTarget.value)}`
+    document.querySelector('#total-price').innerHTML = "";
+    const startInput = document.getElementById("booking_start_date").value;
+    const endInput = document.getElementById("booking_end_date").value;
+    const pricePerDay = document.getElementById("price-per-day").innerHTML;
+
+    let days = (Date.parse(endInput) - Date.parse(startInput)) / (1000 * 60 * 60 * 24);
+    this.priceTarget.textContent = `${pricePerDay * days}$`;
   }
 }
-
-// (end_date.to_date - start_date.to_date).to_i
-
-// Need to be done + ask for help to a TA
-/* For the moment the data-action and data-target have been put
-on the html file hens/show */
